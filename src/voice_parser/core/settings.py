77e9f9f1d3cca2834dead900050686dd6417896c
Niklas <niklas@alyst.se>
2025-09-22 16:04:23 +0200

    dev: no us

--- src/voice_parser/core/settings.py
+++ src/voice_parser/core/settings.py
@@ -13,7 +13,7 @@ class S3Settings(BaseSettings):
 
     aws_access_key_id: str
     aws_secret_access_key: str
-    aws_region: str = "us-east-1"
+    aws_region: str = "eu-north-1"
     s3_bucket_name: str
 
 
